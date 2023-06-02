18a7f4bbc73eebce68a00d9d3eca748dab4d64a6
Dmitry Martynchik <dmitry.martynchik@effective-soft.com>
2023-06-02 21:45:06 +0300

    cleaning today_expenses when the next day begins added

--- internal/service/finance.go
+++ internal/service/finance.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/chucky-1/finance/internal/model"
 	"github.com/chucky-1/finance/internal/repository"
@@ -10,6 +11,10 @@ import (
 
 type Finance interface {
 	Add(ctx context.Context, entry *model.Entry) error
+}
+
+type Cleaner interface {
+	ClearYesterday(ctx context.Context) error
 }
 
 type finance struct {
@@ -31,4 +36,11 @@ func (f *finance) Add(ctx context.Context, entry *model.Entry) error {
 
 	entry.Type = fmt.Sprintf("today_%s", tp)
 	return f.repo.Add(ctx, entry)
+}
+
+func (f *finance) ClearYesterday(ctx context.Context) error {
+	return f.repo.ClearYesterday(ctx, &model.Entry{
+		Type: "today_expenses",
+		Date: time.Now().UTC().Add(-time.Hour),
+	})
 }
